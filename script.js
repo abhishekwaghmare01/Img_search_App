4f25c1551b6bc18838e452e700c901c14ecfe5ff
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2024-12-31 00:48:28 +0530

    Add eventListener to Load more button to fetch more images

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ const accessKey = 'RhCGX3unFQf4a7VgTL1faPUv4kenbaFJ9TH5v6VgqBw';
 const searchFrom = document.querySelector('form');
 const imageContainer = document.querySelector('.images-container');
 const searchInput = document.querySelector('.search-input');
+const loadMoreBtn = document.querySelector('.loadMoreBtn');
+
+let page = 1;
 
 //Function to fetch images using Unflash AIP
-const fetchImages = async (query) =>{
-
-    imageContainer.innerHTML = '';
+const fetchImages = async (query, pageNo) =>{
+    if(pageNo === 1){
+        imageContainer.innerHTML = '';
+    }
      
-    const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=28&client_id=${accessKey}`;
+    const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=28&page=${pageNo}&client_id=${accessKey}`;
 
     const response = await fetch(url);
     const data = await response.json();
@@ -40,9 +44,15 @@ searchFrom.addEventListener('submit',(e) =>{
     e.preventDefault();
     const inputText = searchInput.value.trim();
     if(inputText !== ''){
-        fetchImages(inputText);
+        page = 1;
+        fetchImages(inputText, page);
     }
     else{
         imageContainer.innerHTML = `<h2>Please enter a search Query.</h2>`
     }
+})
+
+//Add eventListener to Load more button to fetch more images
+loadMoreBtn.addEventListener('click', ()=>{
+    fetchImages(searchInput.value.trim(),++page);
 })
