3cc9ad8fffbb20e0c33a92b12273cccd8cf1dced
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2024-12-30 23:01:45 +0530

    Added Event Listener to search Form

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const accessKey = "https://api.unsplash.com/photos/?client_id=RhCGX3unFQf4a7VgTL1faPUv4kenbaFJ9TH5v6VgqBw";
+//const accessKey = "https://api.unsplash.com/photos/?client_id=RhCGX3unFQf4a7VgTL1faPUv4kenbaFJ9TH5v6VgqBw";
 
 const searchFrom = document.querySelector('form');
-const imageContainer = document.querySelector('.image-container');
+const imageContainer = document.querySelector('.images-container');
+const searchInput = document.querySelector('.search-input');
 
-searchFrom.addEventListener('.submit',() =>{
-    
+//Add eventListener to search Form
+searchFrom.addEventListener('submit',(e) =>{
+    e.preventDefault();
+    const inputText = searchInput.ariaValueMax.trim();
+    if(inputText !== ''){
+        fetchImages(inputText);
+    }
+    else{
+        imageContainer.innerHTML = `<h2>Please enter a search Query.</h2>`
+    }
 })
