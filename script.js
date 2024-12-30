de10d36e7e4262118c9c42b5b17ee831a8f9eaa0
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2024-12-30 22:48:22 +0530

    Add Access key

---
+++ script.js
@@ -0,0 +1,8 @@
+const accessKey = "https://api.unsplash.com/photos/?client_id=RhCGX3unFQf4a7VgTL1faPUv4kenbaFJ9TH5v6VgqBw";
+
+const searchFrom = document.querySelector('form');
+const imageContainer = document.querySelector('.image-container');
+
+searchFrom.addEventListener('.submit',() =>{
+    
+})
